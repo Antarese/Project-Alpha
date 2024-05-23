9db516798e18e018ff42272af718ea26a8f2a41b
Antarese <quanglh.kts@gmail.com>
2024-05-23 22:15:38 +1000

    Updated Code to be more modular. Separating the functions and sub-fucntions

--- Characters/Redman/redman.gd
+++ Characters/Redman/redman.gd
@@ -8,7 +8,7 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+"""
 @onready var anim = get_node("Redman_Animation_Player")
 
 func _physics_process(delta):
@@ -41,3 +41,42 @@ func _physics_process(delta):
 	if velocity.y > 0:
 		anim.play("Fall")
 	move_and_slide()
+"""
+@onready var anim = $Redman_Animation_Player
+@onready var sprite = $Redman_Animated_Sprite_2D
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	handle_movement()
+	handle_animations()
+
+func apply_gravity(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_jump():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+func handle_movement():
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	sprite.flip_h = (direction == -1)
+	move_and_slide()
+
+
+func handle_animations():
+	if velocity.y < 0:
+		anim.play("Jump")
+	elif velocity.y > 0:
+		anim.play("Fall")
+	elif velocity.x !=0:
+		anim.play("Run")
+	else:
+		anim.play("Idle")
+
